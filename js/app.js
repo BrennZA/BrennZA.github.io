d89d5b1c71b44bbf03744cc66fbe9b1160cf8792
Brenn_is_working <68723175+BrennZA@users.noreply.github.com>
2024-04-09 19:08:01 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function msgSubmit() {
+    let alert = document.getElementById("alert");
+
+    alert.style.display = "block";
+}
+
+function alertOK() {
+    let alert = document.getElementById("alert");
+
+    alert.style.display = "none";
+}
